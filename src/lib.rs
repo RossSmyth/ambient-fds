565c2891d429a307de6e538fe929a84767eb1182
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2026-08-18 22:57:22 -0400

    add more validation to the unchecked API

--- src/lib.rs
+++ src/lib.rs
@@ -16,13 +16,21 @@ pub mod fd_store;
 #[non_exhaustive]
 #[derive(Debug, Clone)]
 pub enum EnvVarErrorKind {
+    /// The environment variables was not in the environment
     NotFound,
+    /// The variable was not unicode encoded
     NotUnicode(Vec<u8>),
+    /// The variables was supposed to be a number, but was not.
     NotANumber(IntErrorKind),
+    /// Invalid format
+    InvalidFormat(String),
 }
 
+/// Errors related to reading the environment variables.
 #[derive(Debug, Clone)]
 pub struct EnvVarError {
+    /// The name of the environment variable. Do not attempt to fix or set the
+    /// variables yourself, consult systemd documentation.
     pub name: String,
     pub kind: EnvVarErrorKind,
 }
--- src/unchecked.rs
+++ src/unchecked.rs
@@ -8,7 +8,7 @@ use crate::{EnvVarError, EnvVarErrorKind};
 /// Unprocessed or validated FDs
 pub struct RawAmbientFd {
     fd: RawFd,
-    name: Box<str>,
+    name: String,
 }
 
 impl RawAmbientFd {
@@ -25,7 +25,7 @@ impl RawAmbientFd {
 
 /// This is a constant in a systemd header, and people hardcode this. So it's probably fine to do
 /// the same here.
-const SD_LISTEN_FDS_START: i32 = 3;
+const SD_LISTEN_FDS_START: usize = 3;
 
 /// This function doesn't do any checking, just collects.
 /// If there isn't anything to collect, returns an empty Vec.
@@ -63,6 +63,21 @@ pub fn get_raw_ambient_fds() -> Result<Vec<RawAmbientFd>, EnvVarError> {
         }
     };
 
+    let total_fds = SD_LISTEN_FDS_START + 1 - fd_count;
+
+    // It is C brained so we must check.
+    match TryInto::<i32>::try_into(total_fds) {
+        Ok(_) => (),
+        Err(_) => {
+            return Err(EnvVarError {
+                name: "LISTEN_FDS".to_string(),
+                kind: EnvVarErrorKind::InvalidFormat(format!(
+                    "Total number of FDs exceed to possible total."
+                )),
+            });
+        }
+    };
+
     let raw_names = match env::var("LISTEN_FDNAMES") {
         Ok(raw_names) => raw_names,
         Err(VarError::NotPresent) => {
@@ -79,15 +94,25 @@ pub fn get_raw_ambient_fds() -> Result<Vec<RawAmbientFd>, EnvVarError> {
         }
     };
 
-    let name_list = raw_names.split(":");
+    let name_list: Vec<String> = raw_names.split(":").map(str::to_string).collect();
+
+    if name_list.len() != total_fds {
+        return Err(EnvVarError {
+            name: "LISTEN_FDS, LISTEN_FDNAMES".to_string(),
+            kind: EnvVarErrorKind::InvalidFormat(format!(
+                "Number of FDs specified ({total_fds}), and number of names ({}) mismatch",
+                name_list.len()
+            )),
+        });
+    }
 
     Ok((SD_LISTEN_FDS_START..)
         .skip(1) // The start is not counted as an FD
         .take(fd_count)
-        .zip(name_list)
+        .zip(name_list.into_iter())
         .map(|(fd, name)| RawAmbientFd {
-            fd,
-            name: name.into(),
+            fd: fd.try_into().unwrap(),
+            name: name,
         })
         .collect())
 }
